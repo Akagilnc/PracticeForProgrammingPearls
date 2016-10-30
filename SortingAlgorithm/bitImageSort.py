6b1c78abf99d1d7cb5150004d8c5d6da31c085b6
DeronLee <akagilnc@gmail.com>
2016-10-30 17:20:23 +0900

    add comments

--- SortingAlgorithm/bitImageSort.py
+++ SortingAlgorithm/bitImageSort.py
@@ -2,18 +2,22 @@ import random
 
 indexList = []
 inputList = []
-
+# Make 1000000 random number to input
 for i in range(0, 1000000):
     inputList.append(int(random.randrange(0, 10000000)))
 
+# Init the index and set all the element equal to 0
 for i in range(0, 10000000):
     indexList.append(0)
+
 
 for num in inputList:
     indexList[num] += 1
 
+# Open a file to write result
 f = open('resultFile.txt', 'w')
 
+# Write result to file
 for i in range(10000000):
     if indexList[i] > 0:
         f.write((str(i)+'\n') * indexList[i])
