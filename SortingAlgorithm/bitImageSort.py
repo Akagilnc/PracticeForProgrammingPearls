500c70c4240764a088779b494fcaecc573a8b586
DeronLee <akagilnc@gmail.com>
2016-10-30 15:41:40 +0900

    change the name of variable

--- SortingAlgorithm/bitImageSort.py
+++ SortingAlgorithm/bitImageSort.py
@@ -1,22 +1,22 @@
 import random
 
-dict = []
-list = []
+indexList = []
+inputList = []
 
 for i in range(0, 1000000):
-    list.append(int(random.randrange(0, 10000000)))
+    inputList.append(int(random.randrange(0, 10000000)))
 
 for i in range(0, 10000000):
-    dict.append(0)
+    indexList.append(0)
 
-for num in list:
-    dict[num] += 1
+for num in inputList:
+    indexList[num] += 1
 
 f = open('resultFile.txt', 'w')
 
 for i in range(10000000):
-    if dict[i] > 0:
-        f.write((str(i)+'\n')*dict[i])
+    if indexList[i] > 0:
+        f.write((str(i)+'\n') * indexList[i])
 
 f.close()
 
