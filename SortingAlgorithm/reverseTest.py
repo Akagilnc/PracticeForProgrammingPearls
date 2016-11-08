33ec51d151819f9a85165843572187c6815abb63
DeronLee <akagilnc@gmail.com>
2016-11-08 15:40:48 +0800

    add type check and length check

--- SortingAlgorithm/reverseTest.py
+++ SortingAlgorithm/reverseTest.py
@@ -1,4 +1,13 @@
+import sys
+
 tempString = input("Input the string you want to reverse: ")
-i = int(input("input the num of i: "))
+try:
+    i = int(input("input the what number do you wanna to begin reverse: "))
+except (TypeError, ValueError):
+    sys.exit("You must input a integer for i")
+
+if len(tempString) < i:
+    i = len(tempString)
+
 
 print((tempString[i - 1::-1] + tempString[len(tempString):i - 1:-1])[::-1])
