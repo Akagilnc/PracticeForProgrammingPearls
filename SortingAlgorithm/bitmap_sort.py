92d6b16650fd77d978f5d5cb07453b19c500677e
DeronLee <akagilnc@gmail.com>
2017-03-17 23:46:03 +0800

    add comment and change logic with file object to keep it close

--- SortingAlgorithm/bitmap_sort.py
+++ SortingAlgorithm/bitmap_sort.py
@@ -7,28 +7,28 @@ def bitmap_sort():
     for i in range(0, 10000000):
         index_list.append(0)
 
-    # use +1 can make sure we won't miss some equals number.
+    # Make the value = 1 when the number in file.
     for line in open('input_file.txt', 'r'):
-        index_list[int(line)] += 1
+        index_list[int(line)] = 1
 
     # Open a file to write result
-    f = open('result_file.txt', 'w')
-
-    # Write result to file
-    for index, value in enumerate(index_list):
-        if value > 0:
-            f.write((str(index)+'\n') * value)
+    with open('result_file.txt', 'w') as f:
+        # Write result to file
+        for index, value in enumerate(index_list):
+            if value == 1:
+                f.write((str(index)+'\n') * value)
 
 
 def make_input():
-    # Make 1000000 random number to input
+    # Make 10,000,000 unique random number
     input_list = random.sample(range(1000000, 10000000),
                                8000000)
 
-    f_input = open('input_file.txt', 'w')
-    for line in input_list:
-        f_input.write(str(line) + '\n')
-    f_input.close()
+    # Open a input file to store these numbers
+    with open('input_file.txt', 'w') as f_input:
+        # Write numbers to input_file
+        for line in input_list:
+            f_input.write(str(line) + '\n')
 
 # make_input()
 bitmap_sort()
