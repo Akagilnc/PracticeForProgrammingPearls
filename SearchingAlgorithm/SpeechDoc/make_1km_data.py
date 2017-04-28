52f1938a85e42c68426606adf9e305be59f248d8
DeronLee <akagilnc@gmail.com>
2017-04-28 11:42:45 +0800

    change logic make sure evert file should be close after write

--- SearchingAlgorithm/SpeechDoc/make_1km_data.py
+++ SearchingAlgorithm/SpeechDoc/make_1km_data.py
@@ -11,24 +11,26 @@ print("---input_list begin %s seconds ---" % (time.time() - start_time))
 # input_list = random.sample(range(1000000000, 9999999999),
 #                            1000000000)
 print("---input_list end %s seconds ---" % (time.time() - start_time))
-i = 0.1
+p = 0.1
 start_time = time.time()
 temp_list = []
 # Open a input file to store these numbers
-with open('input_file_large.txt', 'a') as f_input:
-    # Write numbers to input_file
-    #for temp_list in input_list.pop(10000):
-    for i in range(1000):
-        for line in range(10**6):
-            temp_list.append(str(random.getrandbits(32)) + ',' + id_generator() + '\n')
-            # temp_word = id_generator()
-            # f_input.write(temp_word + '\t' + temp_word * random.randrange(1, 9) + '\n')
-            if line % 999999 == 0 and line != 0:
-                print("--- %f%% end in %s seconds ---" % (i, time.time() - start_time))
-                i += 0.1
-                start_time = time.time()
 
+# Write numbers to input_file
+#for temp_list in input_list.pop(10000):
+for i in range(1000):
+    for line in range(10**6):
+        temp_list.append(str(random.getrandbits(32)) + ',' + id_generator() + '\n')
+        # temp_word = id_generator()
+        # f_input.write(temp_word + '\t' + temp_word * random.randrange(1, 9) + '\n')
+        if line % 999999 == 0 and line != 0:
+            print("--- %f%% end in %s seconds ---" % (p, time.time() - start_time))
+            p += 0.1
+            start_time = time.time()
+
+    with open('input_file_large.txt', 'a') as f_input:
         start_time = time.time()
         print("--- %s begin in %s seconds ---" % ('write file begin', time.time() - start_time))
         f_input.writelines(temp_list)
         print("--- %s begin in %s seconds ---" % ('write file end', time.time() - start_time))
+
