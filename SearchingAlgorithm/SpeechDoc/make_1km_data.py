037776fc2b14cd538f928995346af3e9fb0fff75
DeronLee <akagilnc@gmail.com>
2017-04-28 19:09:14 +0800

    make 8 temp files to sort

--- SearchingAlgorithm/SpeechDoc/make_1km_data.py
+++ SearchingAlgorithm/SpeechDoc/make_1km_data.py
@@ -3,38 +3,34 @@ import string
 import time
 
 
-def id_generator(size=4, chars=string.ascii_letters + string.digits + string.hexdigits):
-    return ''.join(random.choice(chars) for _ in range(size))
+class MakeData:
+    def __init__(self):
+        self.start_time = time.time()
 
+    def id_generator(self, size=4, chars=string.ascii_letters + string.digits + string.hexdigits):
+        return ''.join(random.choice(chars) for _ in range(size))
 
-def get_number():
-    return str(random.getrandbits(32))
+    def get_number(self):
+        return str(random.getrandbits(32))
 
-start_time = time.time()
-print("---input_list begin %s seconds ---" % (time.time() - start_time))
-# input_list = random.sample(range(1000000000, 9999999999),
-#                            1000000000)
-print("---input_list end %s seconds ---" % (time.time() - start_time))
-p = 0.1
-start_time = time.time()
-temp_list = []
-# Open a input file to store these numbers
+    def get_time(self, message):
+        print('%s in %f seconds' % (message, time.time() - self.start_time))
+        self.start_time = time.time()
 
-# Write numbers to input_file
-# for temp_list in input_list.pop(10000):
-for i in range(1001):
-    for line in range(10**6):
-        number = get_number()
-        temp_list.append(number + ',' + number + 'Description \n')
-        # temp_word = id_generator()
-        # f_input.write(temp_word + '\t' + temp_word * random.randrange(1, 9) + '\n')
-    else:
-        print("--- %f%% end in %s seconds ---" % (p, time.time() - start_time))
-        p += 0.1
-        start_time = time.time()
+    def make_data(self):
+        temp_list, p = [], 0.1
+        # Open a input file to store these numbers
+        # Write numbers to input_file
+        for i in range(1001):
+            for line in range(10**6):
+                number = self.get_number()
+                temp_list.append(number + ',' + number + 'Description \n')
+            else:
+                self.get_time("--- %f%% end ---" % p)
+                p += 0.1
 
-    with open('input_file_large.txt', 'a') as f_input:
-        f_input.writelines(temp_list)
-    print("--- %s begin in %s seconds ---" % ('write file end', time.time() - start_time))
-    temp_list.clear()
+            with open('input_file_large.txt', 'a') as f_input:
+                f_input.writelines(temp_list)
+            self.get_time('write file end')
+            temp_list.clear()
 
--- SearchingAlgorithm/SpeechDoc/sort_data.py
+++ SearchingAlgorithm/SpeechDoc/sort_data.py
@@ -9,12 +9,69 @@ class SortData:
         print('%s in %f seconds' % (message, time.time() - self.start_time))
         self.start_time = time.time()
 
-    with open('input_file_large.txt', 'r') as input_file, open('sort_file_large.txt', 'w') as result_file:
-        lines = input_file.readlines()
-        get_time("read file finished")
-        lines.sort()
-        get_time("sort finished")
-        result_file.writelines(lines)
-        get_time('write result finished')
+    @staticmethod
+    def write_to_temp_file(lines):
+        total = 4375000000
+        temp_1, temp_2, temp_3, temp_4, temp_5, temp_6, temp_7, temp_8 = [], [], [], [], [], [], [], []
+        for line in lines:
+            if int((line.split(','))[0]) < total//8:
+                temp_1.append(line)
+            elif total // 8 <= int((line.split(','))[0]) < total // 8 * 2:
+                temp_2.append(line)
+            elif total // 8 * 2 <= int((line.split(','))[0]) < total // 8 * 3:
+                temp_3.append(line)
+            elif total // 8 * 3 <= int((line.split(','))[0]) < total // 8 * 4:
+                temp_4.append(line)
+            elif total // 8 * 4 <= int((line.split(','))[0]) < total // 8 * 5:
+                temp_5.append(line)
+            elif total // 8 * 5 <= int((line.split(','))[0]) < total // 8 * 6:
+                temp_6.append(line)
+            elif total // 8 * 6 <= int((line.split(','))[0]) < total // 8 * 7:
+                temp_7.append(line)
+            else:
+                temp_8.append(line)
+
+        with open('temp_file_1', 'a') as file1, open('temp_file_2', 'a') as file2, \
+                open('temp_file_3', 'a') as file3, open('temp_file_4', 'a') as file4, \
+                open('temp_file_5', 'a') as file5, open('temp_file_6', 'a') as file6, \
+                open('temp_file_7', 'a') as file7, open('temp_file_8', 'a') as file8:
+                file1.writelines(temp_1)
+                file2.writelines(temp_2)
+                file3.writelines(temp_3)
+                file4.writelines(temp_4)
+                file5.writelines(temp_5)
+                file6.writelines(temp_6)
+                file7.writelines(temp_7)
+                file8.writelines(temp_8)
+
+    def write_to_result(self):
+        for i in range(1, 9):
+            with open('temp_file_'+str(i), 'r') as file, open("sort_file_large.txt", 'a') as result_file:
+                lines = file.readlines()
+                self.get_time("read finished " + str(i))
+                lines.sort()
+                self.get_time("sort finished " + str(i))
+                result_file.writelines(lines)
+                self.get_time('write result finished ' + str(i))
+
+    def main(self):
+        with open('input_file_large.txt', 'r') as input_file:
+            total_line_num = 8*10**9
+            i = 1
+            while True:
+                lines = input_file.readlines(total_line_num // 4)
+                self.get_time("read file finished " + str(i))
+                self.write_to_temp_file(lines)
+                self.get_time("write to temp " + str(i))
+                i += 1
+                if not lines:
+                    break
+
+        self.write_to_result()
+
+    def main2(self):
+        self.write_to_result()
 
 
+sort = SortData()
+sort.main2()
